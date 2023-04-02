390dac445bfe238b6c32c31aee104330cbb75e5c
RGGH <pisscompany@gmail.com>
2023-04-02 21:01:23 +0100

    update comment in main

--- main.py
+++ main.py
@@ -3,10 +3,10 @@
     Author : redandgreen
     version 1.1
 """
-# python3 -m spacy download en_core_web_sm 
+# python3 -m spacy download en_core_web_sm
 
-import uvicorn
 import spacy
+import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
@@ -36,47 +36,30 @@ def vars(data: request_body) -> dict:
     use POST to allow >=255 chars
     """
     mytk = nlp(data.features)
-    res_date = [
-        (token_ent.text) for token_ent in mytk.ents if token_ent.label_ == "DATE"
-    ]
-    res_country = [
-        (token_ent.text) for token_ent in mytk.ents if token_ent.label_ == "GPE"
-    ]
-    res_org = [
-        (token_ent.text) for token_ent in mytk.ents if token_ent.label_ == "ORG"
-    ]
-    res_fac = [
-        (token_ent.text) for token_ent in mytk.ents if token_ent.label_ == "FACILITY"
-    ]
-    res_per = [
-        (token_ent.text) for token_ent in mytk.ents if token_ent.label_ == "PERSON"
-    ]
-    res_mon = [
-        (token_ent.text) for token_ent in mytk.ents if token_ent.label_ == "MONEY"
-    ]
-    res_qua = [
-        (token_ent.text) for token_ent in mytk.ents if token_ent.label_ == "QUANTITY"
-    ]
-    res_woa = [
-        (token_ent.text) for token_ent in mytk.ents if token_ent.label_ == "WORK_OF_ART"
-    ]
-    res_ord = [
-        (token_ent.text) for token_ent in mytk.ents if token_ent.label_ == "ORDINAL"
-    ]
+    res_date = [(token_ent.text) for token_ent in mytk.ents if token_ent.label_ == "DATE"]
+    res_country = [(token_ent.text) for token_ent in mytk.ents if token_ent.label_ == "GPE"]
+    res_org = [(token_ent.text) for token_ent in mytk.ents if token_ent.label_ == "ORG"]
+    res_fac = [(token_ent.text) for token_ent in mytk.ents if token_ent.label_ == "FACILITY"]
+    res_per = [(token_ent.text) for token_ent in mytk.ents if token_ent.label_ == "PERSON"]
+    res_mon = [(token_ent.text) for token_ent in mytk.ents if token_ent.label_ == "MONEY"]
+    res_qua = [(token_ent.text) for token_ent in mytk.ents if token_ent.label_ == "QUANTITY"]
+    res_woa = [(token_ent.text) for token_ent in mytk.ents if token_ent.label_ == "WORK_OF_ART"]
+    res_ord = [(token_ent.text) for token_ent in mytk.ents if token_ent.label_ == "ORDINAL"]
 
-    
-
-    return {"date": res_date, 
-            "country": res_country, 
-            "organization":res_org,
-            "facility":res_fac, 
-            "people":res_per, 
-            "money":res_mon, 
-            "weight_distance":res_qua,
-            "work_of_art":res_woa,
-            "ordinal":res_ord
+    return {
+        "date": res_date,
+        "country": res_country,
+        "organization": res_org,
+        "facility": res_fac,
+        "people": res_per,
+        "money": res_mon,
+        "weight_distance": res_qua,
+        "work_of_art": res_woa,
+        "ordinal": res_ord,
     }
 
 
+# uvicorn main:app --reload
+# or
 # if __name__ == "__main__":
 #     uvicorn.run("app.api:app", host="0.0.0.0", port=8000, reload=True)
