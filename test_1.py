82bdcc4983a0362813afa211fe09945807eaeaf7
RGGH <pisscompany@gmail.com>
2023-04-02 22:42:04 +0100

    Amend test to check local url

--- test_1.py
+++ test_1.py
@@ -1,10 +1,14 @@
 import requests
+from pprint import pprint
 
-url = "https://api-4-mmorfk42na-nw.a.run.app/ents"
-#url = "http://127.0.0.1:8000/ents"
+# This was deployed on GCP in 2021 - example url :
+#url = "https://api-4-mmorfk42na-nw.a.run.app/ents"
+
+# Test for localhost
+url = "http://127.0.0.1:8000/ents"
 
 params = {"features":"The Microsoft book written by Hayden Liu in 2018 was sold at $30 in America in 2019 by the Statue of liberty , it had a flower on the cover"}
 
 answer = requests.post(url, json = params)
 
-print(answer.text)
+pprint(answer.json())
